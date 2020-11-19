04290bada3068bd33e46b381208760ea029a91fb
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2020-11-19 12:51:21 +0100

    get rid of pbr for now

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import setuptools
+from setuptools.command.install import install
+
+
+class InstallWithCompile(install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+
+        compiler = compile_catalog(self.distribution)
+        option_dict = self.distribution.get_option_dict("compile_catalog")
+        compiler.domain = [option_dict["domain"][1]]
+        compiler.directory = option_dict["directory"][1]
+        compiler.run()
+        super().run()
+
 
 setuptools.setup(
-    setup_requires=["pbr>=2.0.0"],
-    pbr=True,
+    setup_requires=[
+        "Babel>=2.9.0",
+    ],
+    cmdclass={"install": InstallWithCompile},
     package_dir={"": "src"},
+    package_data={"": ["locale/*/*/*.mo", "locale/*/*/*.po"]},
     packages=setuptools.find_packages(where="src"),
 )
--- src/warmahordes_opendata/_i18n.py
+++ src/warmahordes_opendata/_i18n.py
@@ -1,8 +1,16 @@
+from importlib import resources
+import os
+
 import oslo_i18n
 
 DOMAIN = "warmahordes_opendata"
 
 oslo_i18n.enable_lazy()
+
+with resources.path(__package__, "locale") as localedir:
+    os.environ[
+        oslo_i18n._locale.get_locale_dir_variable_name(__package__)
+    ] = str(localedir)
 
 _translators = oslo_i18n.TranslatorFactory(domain=DOMAIN)
 
