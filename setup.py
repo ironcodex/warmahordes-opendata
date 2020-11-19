ba0324852d313ab832005e2bc4c9d79b1033a2a2
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2020-11-19 23:49:00 +0100

    restore pbr and commit translations

--- setup.py
+++ setup.py
@@ -1,28 +1,11 @@
 import setuptools
-from setuptools.command.install import install
-
-
-class InstallWithCompile(install):
-    def run(self):
-        from babel.messages.frontend import compile_catalog
-
-        compiler = compile_catalog(self.distribution)
-        option_dict = self.distribution.get_option_dict("compile_catalog")
-        compiler.domain = [option_dict["domain"][1]]
-        compiler.directory = option_dict["directory"][1]
-        compiler.run()
-        super().run()
-
 
 setuptools.setup(
     setup_requires=[
         "Babel>=2.9.0",
+        "pbr>=2.0.0",
     ],
-    install_requires=[
-        "oslo.i18n>=5.0.1"
-    ],
-    cmdclass={"install": InstallWithCompile},
+    pbr=True,
     package_dir={"": "src"},
-    package_data={"": ["locale/*/*/*.mo", "locale/*/*/*.po"]},
     packages=setuptools.find_packages(where="src"),
 )
