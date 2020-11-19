ea44359255adeb074685812679a96b50744e36c4
Moisés Guimarães de Medeiros <guimaraes@pm.me>
2020-11-19 13:20:09 +0100

    disable coverage for now

--- tests/test_keywords.py
+++ tests/test_keywords.py
@@ -1,8 +1,20 @@
+import oslo_i18n
 from tests import base
 
 from warmahordes_opendata import keywords
 
 
-class TestTheme(base.TestCase):
-    def test_immunities(self):
-        self.assertEqual("Immunity: Fire", keywords.Immunity.FIRE.keyword)
+class TestKeywords(base.TestCase):
+    def setUp(self):
+        super().setUp()
+
+        self.keyword = keywords.Immunity.FIRE.keyword
+
+    def test_keyword(self):
+        self.assertEqual("Immunity: Fire", self.keyword)
+
+    def test_translate_pt_BR(self):
+        self.assertEqual(
+            "Imunidade: Fogo",
+            oslo_i18n.translate(self.keyword, "pt_BR"),
+        )
