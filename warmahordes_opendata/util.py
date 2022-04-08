00c14a624e62997a965ddd6b7f8ce9f618118c10
Moisés Guimarães de Medeiros <contact@moisesguimaraes.com>
2022-04-08 11:49:23 +0200

    adding docs to util

--- warmahordes_opendata/util.py
+++ warmahordes_opendata/util.py
@@ -23,11 +23,20 @@ __ROOT = resources.files(__package__)
 
 
 @cache
-def slugify(value):
-    return __slgf(value, separator="_", replacements=(("&", "and"),))
+def slugify(text: str) -> str:
+    """Makes a slug from the given text"""
+    return __slgf(text, separator="_", replacements=(("&", "and"),))
 
 
-def keywords_filter(collection, keywords):
+def keywords_filter(collection: list, keywords: list) -> list:
+    """Filters a list of elements based on a list of keywords.
+
+    :param collection: The collection to be filtered.
+    :param keywords: A list of keywords to be used when fitering the collection.
+
+    :returns: A list composed of all elements in the original collection that
+    contain all required keywords.
+    """
     for word in keywords:
         collection = list(filter(lambda x: word in x, collection))
 
@@ -35,13 +44,29 @@ def keywords_filter(collection, keywords):
 
 
 @cache
-def load_file(*path, root=__ROOT):
+def load_file(*path: tuple, root: str = __ROOT) -> yaml.YAMLObject:
+    """Loads a yaml file using yaml.safe_load.
+
+    :param path: Relative path to the yaml file.
+    :param root: Where the relative path should start from, defaults to package path.
+
+    :returns: An object of the class mapped in the yaml file.
+    """
     with open(os.path.join(root, *path), "r") as fd:
         return yaml.safe_load(fd.read())
 
 
 @cache
-def load_dir(*path, root=__ROOT):
+def load_dir(*path: tuple, root: str = __ROOT) -> dict:
+    """Loads all yaml files recursively under a specific path.
+
+    :param path: Relative path to the starting directory.
+    :param root: Where the relative path should start from, defaults to package path.
+
+    :returns: A dictionary with the content of all yaml files found. Sub directories
+              and file names are part of the dictionary's structure matching the file
+              system tree.
+    """
     root, dirs, files = next(os.walk(os.path.join(root, *path)))
     result = dict()
 
@@ -55,15 +80,20 @@ def load_dir(*path, root=__ROOT):
     return result
 
 
-def flatten(tree, key=None):
+def flatten(tree: dict, key: str = "") -> dict:
+    """Flattens a multi level dictionary tree to a single level.
+
+    :param tree: The dictionary to be flattened.
+    :param key: An attribute name in the leaf object to be used as the new key.
+
+    :returns: A dictionary contains all leaf elements found recursively in the tree.
+    """
     result = dict()
 
     for k, v in tree.items():
         if type(v) is dict:
             result.update(flatten(v, key))
-        elif key is not None:
-            result[getattr(v, key)] = v
         else:
-            result[k] = v
+            result[getattr(v, key) if key else k] = v
 
     return result
